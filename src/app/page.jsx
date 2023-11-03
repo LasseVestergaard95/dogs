75c8f7cac528bddae8f729c9012493561b3c448e
Lasse Vestergaard <lassevestergaard95@hotmail.com>
2023-11-03 09:21:12 +0100

    random hund virker

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,18 @@
-export const dynamic= "force-dynamic"
+export const dynamic = "force-dynamic";
 
-export const metadata={
-  title:"Home",
+export const metadata = {
+  title: "Home",
   description: "A homepage",
-}
+};
 
-const res = await fetch("https://dog.ceo/api/breeds/image/random");
+export default async function Home() {
+  const res = await fetch("https://dog.ceo/api/breeds/image/random");
   const data = await res.json();
-
-console.log(data)
-export default function Home() {
+  console.log(data);
   return (
     <main>
       <h1>Hej</h1>
       <img src={data.message} alt="" />
     </main>
-  )
+  );
 }
